7542e06cf71e17beb00004a7e42ab1b0666adc75
Nazmul Hasan <46302179+Nazmul1850@users.noreply.github.com>
2025-01-19 14:38:31 +0600

    Operators

---
+++ Class1/variable.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int main() {
+    // char a;
+    // printf("Enter your Charecter:");
+    // scanf("%c", &a);
+    // printf("The Char is %c", a);
+
+// For int
+    // int x;
+    // printf("\nEnter your Integer:");
+    // scanf("%d", &x);
+    // printf("The int is %d", x);
+    // return 0;
+
+    // Type Declaration
+
+    // int a = 22;
+    // int b = a;
+    // int c = b + 1;
+    // int d = 1, e;
+
+    // printf("a = %d,\n b = %d,\n c = %d,\n d = %d, e = %d", a, b, c, d, e);
+
+
+    // float x;
+    // float y = 11.75;
+
+    // x = 4*3/6*2+3*3/6.0;
+
+    // printf("Add %f", x);
+/*
+    int a = 5;
+    int b = 3;
+
+    if (-a < b) {
+        printf("True");
+    }else {
+        printf("False");
+    }
+*/
+
+/*    int bulb1 = 0;
+    int bulb2 = 0;
+
+    if(bulb1 && bulb2) {
+        printf("Both Bulb On");
+    }else if (bulb1 || bulb2) {
+        printf("One is On");
+    }else {
+        printf("Both are off");
+    }
+*/
+
+    int a = 5;
+    a /= 5;
+
+
+
+}
+
