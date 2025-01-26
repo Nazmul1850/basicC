3ac93a1377ac586e9213ce08dc7c0b1dff42d33a
Nazmul Hasan <46302179+Nazmul1850@users.noreply.github.com>
2025-01-26 14:27:37 +0600

    Basic Problem Solve class

---
+++ Class 2/assignement1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#define PI 3.14
+
+int main(){
+
+    // printf("The question is- \"how to write a \\comment/ in C programming language?\"");
+
+    // int a,b,c;
+    // scanf("%d%*d%d", &a, &c);
+    // printf("First Value: %d, Middle Value, %d, Last Value %d", a, b, c);
+
+    
+    // unsigned long int li = 2147483647;
+    // long long int lli = 9223372036854775807;
+    // long double ld = 3.14e+2;
+    // short int si = 10;
+
+    // printf("%lu\n", li);
+    // printf("%lld\n", lli);
+    // printf("%Lf\n", ld); Please work on it
+    // printf("%hd", si);
+
+
+    // printf("Pi = %f", PI);
+    float f = 123.098000;
+    float f1 = 12.11;
+    printf("Value = %10.2f\n",f);
+    printf("Value = %10.2f",f1);
+
+
+    return 0;
+}
